32d46bfb35dde1265255fed41817720fe550ee9a
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2017-01-17 10:59:10 -0500

    Setup JS file

---
+++ app/assets/javascripts/images/show.js
@@ -0,0 +1,26 @@
+
+var IMG = {};
+
+IMG.model = (function(){
+
+  return {};
+
+}());
+
+IMG.view = (function($){
+
+  return {};
+
+}($));
+
+IMG.controller = (function(model, view){
+
+  return {};
+
+}(IMG.model, IMG.view));
+
+$(document).ready(function(){
+  if ($("body").data("controller-action") === "images-show"){
+    // TODO
+  }
+});
